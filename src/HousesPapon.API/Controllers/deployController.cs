66d6cd518ceb61afd1fe754245499b011eacee62
Rafael <rbezerra874@gmail.com>
2025-06-13 16:04:00 -0300

    add health health controller

---
+++ src/HousesPapon.API/Controllers/deployController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HousesPapon.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("health")]
+    public class deployController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("ok");
+    }
+}
